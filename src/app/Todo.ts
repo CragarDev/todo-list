018a12f5c5b41ec5f20f3b0a1d4e76ad1cc810ae
Craig Burke <cragardev@gmail.com>
2022-06-21 15:54:19 -0500

    created temp mock data

---
+++ src/app/Todo.ts
@@ -0,0 +1,10 @@
+export interface Todo {
+    id?: number;
+    title: string;
+    dueDate: string;
+    discription: string;
+    completed: boolean;
+}
+//# sourceMappingURL=todo.js.map
+
+
--- src/app/mock-todos.ts
+++ src/app/mock-todos.ts
@@ -2,16 +2,21 @@ export const ToDos = [
     {
         id: 1,
         title: 'Learn Angular',
+        dueDate: 'June 21, 2022',
+        discription: 'Learn Angular and its framework',
         completed: false
     },
     {
         id: 2,
         title: 'Learn TypeScript',
+        dueDate: 'June 22, 2022',
+        discription: 'Learn to use TypeScript',
         completed: false
     },
     {
         id: 3,
         title: 'Learn Node',
+        dueDate: 'March 23, 2022',
         completed: true
     }
 ];
