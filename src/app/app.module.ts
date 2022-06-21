3aec2055a85e7b6ce4e5eac9d86f94315d0447a3
Craig Burke <cragardev@gmail.com>
2022-06-21 16:10:11 -0500

    generated todos comp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ButtonComponent } from './components/button/button.component';
+import { TodosComponent } from './components/todos/todos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ButtonComponent
+    ButtonComponent,
+    TodosComponent
   ],
   imports: [
     BrowserModule
