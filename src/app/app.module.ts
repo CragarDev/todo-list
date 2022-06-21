d505e02fcca66efab09c45a4b20c82a133f95b36
Craig Burke <cragardev@gmail.com>
2022-06-21 18:14:19 -0500

    added HttpClient and set it up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -18,7 +19,8 @@ import { TodoItemComponent } from './components/todo-item/todo-item.component';
   ],
   imports: [
     BrowserModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -1,17 +1,19 @@
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs'
+import { HttpClient, HttpHeaders} from '@angular/common/http';
+import { Observable } from 'rxjs'
 import { Todo } from '../Todo';
-import { TODOS } from '../mock-todos';
+// import { TODOS } from '../mock-todos';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
+  private apiUrl = 'http://localhost:5000/todos';
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
 
   getTodos(): Observable<Todo[]> {
-    const todos = of(TODOS);
-    return todos;
+    // const todos = of(TODOS);
+    return this.http.get<Todo[]>(this.apiUrl);
   }
 }
