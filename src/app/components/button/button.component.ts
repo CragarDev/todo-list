5d3b543a2bc89ba247ce1feca3a0eac9d3593e98
Craig Burke <cragardev@gmail.com>
2022-06-21 14:37:38 -0500

    added select styling to add button

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -6,11 +6,17 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./button.component.css']
 })
 export class ButtonComponent implements OnInit {
+  @Input() text: string = '';
+  @Input() color: string = '';
 
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onClick() {
+    console.log('clicked');
   }
 
 }
