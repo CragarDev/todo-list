e6ed3914ffdf4ca027002f67e8ad9608baa8617b
Craig Burke <cragardev@gmail.com>
2022-06-21 21:53:52 -0500

    changed to toggle button to show only on the home page

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UiService } from '../../services/ui.service';
 import { Subscription } from 'rxjs';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -12,7 +13,7 @@ export class HeaderComponent implements OnInit {
   showAddTodo: boolean = false;
   subscription: Subscription;
 
-  constructor(private uiService:UiService) {
+  constructor(private uiService:UiService, private router:Router) {
     this.subscription = this.uiService.onToggle().subscribe((value) => (this.showAddTodo = value));
    } // runs when object is initialized
 
@@ -21,6 +22,10 @@ export class HeaderComponent implements OnInit {
 
   toggleAddTodo() {
     this.uiService.toggleAddTodo();
+  }
+
+  hasRoute(route:string) {
+    return this.router.url === route;
   }
 
 }
